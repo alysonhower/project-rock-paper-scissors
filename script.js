c5d10a698edd755a720cb4b04329657039ed7d07
Alysonhower <alysonhower@gmail.com>
2022-07-20 11:57:30 -0300

    Add video and minor changes

--- script.js
+++ script.js
@@ -6,15 +6,25 @@ const computerPlay = () => {
   return selection
 }
 
+document.querySelector('.tie').classList.toggle('hidden')
+document.querySelector('.win').classList.toggle('hidden')
+document.querySelector('.loose').classList.toggle('hidden')
 
 const playRound = (playerSelection, computerSelection) => {
   if (playerSelection === "rock" && computerSelection === "rock") {
+    roundCounter++
+    document.querySelector('.chooseYourWeapon').classList.toggle('hidden')
+    document.querySelector('.tie').classList.toggle('hidden')
     console.log("Tie")
   } else if
     (playerSelection === "rock" && computerSelection === "paper") {
+    document.querySelector('.chooseYourWeapon').classList.toggle('hidden')
+    document.querySelector('.loose').classList.toggle('hidden')
     console.log("You loose")
   }
   else {
+    document.querySelector('.chooseYourWeapon').classList.toggle('hidden')
+    document.querySelector('.win').classList.toggle('hidden')
     console.log("You win")
   }
 
